77ed56bcc4e27b0ff23034708fe8ead24fc98196
Kenny Chirombo <chirombokenny@gmail.com>
2024-01-20 10:37:56 +0200

    add cache manager configuration

--- apps/request-handler/src/request-handler.module.ts
+++ apps/request-handler/src/request-handler.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RequestHandlerController } from './request-handler.controller';
 import { RequestHandlerService } from './request-handler.service';
 import { UniqueIdGeneratorModule } from '@aggregator/unique-id-generator';
+import { CacheManagerModule } from '@aggregator/cache-manager';
 
 @Module({
-  imports: [UniqueIdGeneratorModule],
+  imports: [UniqueIdGeneratorModule, CacheManagerModule],
   controllers: [RequestHandlerController],
   providers: [RequestHandlerService],
 })
---
+++ libs/cache-manager/src/cache-manager.module.ts
@@ -0,0 +1,43 @@
+import { Module } from '@nestjs/common';
+import { CacheManagerService } from './cache-manager.service';
+import { RedisModule, RedisModuleOptions } from '@liaoliaots/nestjs-redis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+/*
+ * The CacheManagerModule is responsible for creating a CacheManagerService
+ * instance and exporting it for use in other modules.
+ *
+ * The CacheManagerModule uses the RedisModule to create a Redis client
+ * instance. The Redis client is configured using the REDIS_URL environment
+ * variable. If the REDIS_URL environment variable is not set, the Redis client
+ * will default to connecting to localhost:6379.
+ *
+ * The CacheManagerModule uses the ConfigModule to access environment variables
+ * and the ConfigService to access environment variables.
+ *
+ * The CacheManagerModule is imported by the RequestHandlerModule.
+ */
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (
+        configService: ConfigService,
+      ): Promise<RedisModuleOptions> => {
+        const url =
+          configService.get<string>('REDIS_URL') || 'redis://localhost:6379';
+        return {
+          config: {
+            url,
+          },
+        };
+      },
+    }),
+  ],
+  providers: [CacheManagerService],
+  exports: [CacheManagerService],
+})
+export class CacheManagerModule {}
